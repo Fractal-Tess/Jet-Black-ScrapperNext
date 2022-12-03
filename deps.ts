009caa1c29208a9b8d1fede461d4e6138fe4af19
Fractal-Tess <vgfractal@gmail.com>
2022-12-04 00:06:08 +0200

    refactored imports

--- deps.ts
+++ deps.ts
@@ -1,7 +1,8 @@
 // export * from 'https://deno.land/x/cheerio@1.0.7/mod.ts'
-export { yellow, green, red, bold } from 'https://deno.land/std@0.136.0/fmt/colors.ts'
+export * as colors from 'https://deno.land/std@0.167.0/fmt/colors.ts'
+export * as log from 'https://deno.land/std@0.167.0/log/mod.ts'
+export * as path from 'https://deno.land/std@0.167.0/path/mod.ts'
 
-export * as log from 'https://deno.land/std@0.136.0/log/mod.ts'
 export { config } from 'https://deno.land/x/dotenv@v3.2.0/mod.ts'
 export { default as Surreal } from 'https://deno.land/x/surrealdb@v0.5.0/mod.ts'
 export { Subject } from 'https://deno.land/x/rxjs@v1.0.2/mod.ts'
