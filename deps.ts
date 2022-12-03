c6379e9cd810fcc6329168c20da9f6f4633ac807
Fractal-Tess <vgfractal@gmail.com>
2022-12-03 19:00:22 +0200

    more deps

--- deps.ts
+++ deps.ts
@@ -3,3 +3,13 @@ export { yellow, green, red, bold } from 'https://deno.land/std@0.136.0/fmt/colo
 
 export * as log from 'https://deno.land/std@0.136.0/log/mod.ts'
 export { config } from 'https://deno.land/x/dotenv@v3.2.0/mod.ts'
+export { default as Surreal } from 'https://deno.land/x/surrealdb@v0.5.0/mod.ts'
+export { Subject } from 'https://deno.land/x/rxjs@v1.0.2/mod.ts'
+
+// Oak API
+export * from 'https://deno.land/x/oak@v10.5.1/mod.ts'
+// Oak helper
+export * from 'https://deno.land/x/oak@v10.5.1/helpers.ts'
+
+// Cors for oak
+export * from 'https://deno.land/x/cors@v1.2.2/mod.ts'
