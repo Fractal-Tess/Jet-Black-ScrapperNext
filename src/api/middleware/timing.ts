4e58bd89026ee8dcff90116017781c45adf2e7f6
Fractal-Tess <vgfractal@gmail.com>
2022-12-04 00:08:48 +0200

    timing middleware

--- src/api/middleware/timing.ts
+++ src/api/middleware/timing.ts
@@ -1,12 +1,8 @@
-import { log, Middleware } from '@deps'
-
-const logger = log.getLogger('performanceLogger')
+import { Middleware } from '@deps'
 
 export const timing: Middleware = async (ctx, next) => {
   const start = Date.now()
   await next()
   const ms = Date.now() - start
   ctx.response.headers.set('X-Response-Time', `${ms}ms`)
-  const msg = `${ctx.request.ip} => ${ctx.request.url.href} | Took ${ms}ms`
-  logger.info(msg)
 }
