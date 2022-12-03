cada8fd09e636457aa67baf1c41a959b45e9607d
Fractal-Tess <vgfractal@gmail.com>
2022-12-03 05:02:57 +0200

    setting up custom logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,103 +1,14 @@
-import { yellow, green, red, bold, log } from '@deps'
+import * as path from 'https://deno.land/std@0.166.0/path/mod.ts'
+import { IdentifierKind, IdentifierName, IdentifierType } from './types/scrapper.ts'
 
-const msgFormatter = (log: log.LogRecord, msg: string) => {
-  if (log.args.length != 0) {
-    log.args.forEach(arg => {
-      msg += `\n\t=> ${arg},`
-    })
-    msg = msg.slice(0, -1)
+export class CustomLogger {
+  logger
+  constructor(idenKind: IdentifierKind, idenName: IdentifierName, idenType: IdentifierType) {
+    const fUrl = path.fromFileUrl(new URL('.', import.meta.url).href)
+    const logsDir = path.join(fUrl, `../logs/${idenKind}/${idenName}/${idenType}`)
+    Deno.mkdirSync(logsDir, { recursive: true })
+
+    console.log(logsDir)
+    this.logger = ''
   }
-  return msg
-}
-
-const fileFormatter = (log: log.LogRecord) => {
-  const msg = `[${log.datetime.toJSON()}]:` + `[${log.levelName}]` + ` - ${log.msg}`
-  return msgFormatter(log, msg)
-}
-
-const consoleFormatter = (log: log.LogRecord) => {
-  const msg = `${log.levelName} ${log.msg}`
-  return msgFormatter(log, msg)
-}
-
-class MyConsoleHandler extends log.handlers.BaseHandler {
-  override format(logRecord: log.LogRecord): string {
-    let msg = super.format(logRecord)
-    switch (logRecord.level) {
-      case log.LogLevels.INFO:
-        msg = green(msg)
-        break
-      case log.LogLevels.WARNING:
-        msg = yellow(msg)
-        break
-      case log.LogLevels.ERROR:
-        msg = red(msg)
-        break
-      case log.LogLevels.CRITICAL:
-        msg = bold(red(msg))
-        break
-      default:
-        break
-    }
-
-    return msg
-  }
-  override log(msg: string): void {
-    console.log(msg)
-  }
-}
-
-class MyFileHandler extends log.handlers.FileHandler {
-  override handle(logRecord: log.LogRecord) {
-    if (this.level > logRecord.level) return
-    const msg = this.format(logRecord)
-    this.log(msg)
-    this.flush()
-  }
-}
-
-export const initLog = async (logMode: 'overwrite' | 'append' = 'append') => {
-  const mode = logMode === 'overwrite' ? 'w' : 'a'
-  log.info(`Initializing logger with mode ${mode}`)
-  const handlers = {
-    genericLogger: new MyFileHandler('DEBUG', {
-      formatter: fileFormatter,
-      mode,
-      filename: 'logs/generic/logs.log'
-    }),
-
-    httpLogger: new MyFileHandler('DEBUG', {
-      formatter: fileFormatter,
-      mode,
-      filename: 'logs/http/http.log'
-    }),
-
-    performanceLogger: new MyFileHandler('DEBUG', {
-      formatter: fileFormatter,
-      mode,
-      filename: 'logs/performance/performance.log'
-    }),
-
-    consoleLogger: new MyConsoleHandler('DEBUG', {
-      formatter: consoleFormatter
-    })
-  }
-
-  await log.setup({
-    handlers,
-    loggers: {
-      default: {
-        level: 'DEBUG',
-        handlers: ['genericLogger', 'consoleLogger']
-      },
-      performanceLogger: {
-        level: 'DEBUG',
-        handlers: ['performanceLogger']
-      },
-      httpLogger: {
-        level: 'DEBUG',
-        handlers: ['httpLogger', 'consoleLogger']
-      }
-    }
-  })
 }
